277ab1e0fac2dd35755c851133c5c1ef98292dc7
Jaka Maver <jmaver@plume.com>
2023-01-08 07:57:40 +0100

    feat: create new /hello endpoint

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/ping", (req, res) => {
   res.send("pong");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
